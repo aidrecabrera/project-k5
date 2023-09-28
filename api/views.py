a0df18f8b2e9e8dad75909429887dd208fae600a
Svene <aidrecabrera@gmail.com>
2023-09-29 04:54:39 +0800

    MongoDB Programmatic Querying Test

--- api/views.py
+++ api/views.py
@@ -3,12 +3,19 @@ from rest_framework.decorators import api_view
 import pymongo
 
 
+import pymongo
+
 @api_view(['GET'])
 def getdata_ph_co2(request):
     client = pymongo.MongoClient('mongodb+srv://svenesismar:a4JvXYZg4PdHpo6m@projectk5cluster.xx0tioc.mongodb.net/')
     k5_database = client['k5-cluster']
     collection = k5_database['api_test']
-    ph_co2 = collection.find({})
-    for document in ph_co2:
-        print(document['year'])
-    return Response("hello")
+
+    documents = collection.find({})
+
+    projected_documents = []
+    for document in documents:
+        projected_documents.append({"population": document["population"]})
+
+    return Response(projected_documents)
+
--- project_k5/wsgi.py
+++ project_k5/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for project_k5 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
 
 import os
 
