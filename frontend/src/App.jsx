0662a073323da822c8a27f536289a4c9e696299d
Svene <aidrecabrera@gmail.com>
2023-10-18 06:54:52 +0800

    Added a navigation bar to the top of the screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import NavigationBar from "./header/NavigationBar";
 import Home from "./main/home/Home";
 function App() {
   return (
     <>
+      <nav className="navigation">
+        <NavigationBar />
+      </nav>
       <body>
         <Home />
       </body>
--- frontend/src/main/home/Home.jsx
+++ frontend/src/main/home/Home.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
-import NavigationBar from "../../header/NavigationBar";
 import "./Home.css";
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <div className="navigation">
-          <NavigationBar />
-        </div>
         <div className="home pt-20 flex items-center justify-center">
           <div className="flex flex-col gap-10">
             <h1 className="text-center font-grotesk text-5xl font-light ">
