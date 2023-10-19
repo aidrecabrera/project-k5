70d48804b268c9f5ba636f1c51f9a345b353fe1f
Svene <aidrecabrera@gmail.com>
2023-10-19 12:29:15 +0800

    Test

--- backend/project_k5/settings.py
+++ backend/project_k5/settings.py
@@ -1,9 +1,10 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+from project_k5.settings import BASE_DIR
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'project_k5.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'frontend/build')]
+        'DIRS': [os.path.join(BASE_DIR_BACKEND)]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -125,7 +126,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build/static')
+    os.path.join(BASE_DIR_FRONTEND, 'frontend/build/'),
 ]
 
 # Default primary key field type
